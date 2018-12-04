bcef0de9c748a7aeaeeba2e1db379fe9fb7fbbd6
Stephen Fairbanks <biggste2000@hotmail.com>
2018-12-04 09:49:32 +0000

    Refactor: Modernise set up - now includes sass-loader

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,33 +1,47 @@
+import sky from "../assets/images/sky.png";
+import ground from "../assets/images/platform.png";
+import star from "../assets/images/star.png";
+import bomb from "../assets/images/bomb.png";
+import player1 from "../assets/images/player1.png";
+
 class BootScene extends Phaser.Scene {
-    constructor() {
-        super({
-            key: 'BootScene'
-        });
-    }
-    preload() {
-        this.load.image('sky', '../../assets/images/sky.png');
-        this.load.image('ground', '../../assets/images/platform.png');
-        this.load.image('star', '../../assets/images/star.png');
-        this.load.image('bomb', '../../assets/images/bomb.png');
-        this.load.spritesheet('player1', '../../assets/images/player1.png', { frameWidth: 32, frameHeight: 48 });
+  constructor() {
+    super({
+      key: "BootScene"
+    });
+  }
+  preload() {
+    this.load.image("sky", sky);
+    this.load.image("ground", ground);
+    this.load.image("star", star);
+    this.load.image("bomb", bomb);
+    this.load.spritesheet("player1", player1, {
+      frameWidth: 32,
+      frameHeight: 48
+    });
 
-        const progress = this.add.graphics();
-       
-        // Register a load progress event to show a load bar
-        this.load.on('progress', (value) => {
-            progress.clear();
-            progress.fillStyle(0xffffff, 1);
-            progress.fillRect(0, this.sys.game.config.height / 2, this.sys.game.config.width * value, 60);
-        });
+    const progress = this.add.graphics();
 
-        // Register a load complete event to launch the title screen when all files are loaded
-        this.load.on('complete', () => {
-            // prepare all animations, defined in a separate file
-            //makeAnimations(this);
-            progress.destroy();
-            this.scene.start('TitleScene');
-        });
-    }
+    // Register a load progress event to show a load bar
+    this.load.on("progress", value => {
+      progress.clear();
+      progress.fillStyle(0xffffff, 1);
+      progress.fillRect(
+        0,
+        this.sys.game.config.height / 2,
+        this.sys.game.config.width * value,
+        60
+      );
+    });
+
+    // Register a load complete event to launch the title screen when all files are loaded
+    this.load.on("complete", () => {
+      // prepare all animations, defined in a separate file
+      //makeAnimations(this);
+      progress.destroy();
+      this.scene.start("TitleScene");
+    });
+  }
 }
 
 export default BootScene;
