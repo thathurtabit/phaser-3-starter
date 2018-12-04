8c9247039981b20ff9d4342e2919b0f369418c60
Stephen Fairbanks <biggste2000@hotmail.com>
2018-12-04 07:56:37 +0000

    Chore: Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,42 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 // Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser/')
-var phaser = path.join(phaserModule, 'src/phaser.js')
+var phaserModule = path.join(__dirname, "/node_modules/phaser/");
+var phaser = path.join(phaserModule, "src/phaser.js");
 
 var definePlugin = new webpack.DefinePlugin({
-    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "true")),
     WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
     CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
-})
+});
 
 module.exports = {
     entry: {
-        app: [
-            'babel-polyfill',
-            path.resolve(__dirname, 'src/main.js')
-        ],
-        vendor: ['phaser']
+        app: ["@babel/polyfill", path.resolve(__dirname, "src/main.js")],
+        vendor: ["phaser"]
     },
-    devtool: 'cheap-source-map',
+    devtool: "cheap-source-map",
     output: {
-      pathinfo: true,
-        path: path.resolve(__dirname, 'dev'),
-        publicPath: './dev/',
-        library: '[name]',
-        libraryTarget: 'umd',
-        filename: '[name].js'
+        pathinfo: true,
+        path: path.resolve(__dirname, "dev"),
+        publicPath: "./dev/",
+        library: "[name]",
+        libraryTarget: "umd",
+        filename: "[name].js"
     },
-    watch: true, 
+    watch: true,
     plugins: [
         definePlugin,
         //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */ }),
         new HtmlWebpackPlugin({
-            filename: '../index.html',
-            template: './src/index.html',
-            chunks: ['vendor', 'app'],
-            chunksSortMode: 'manual',
+            filename: "../index.html",
+            template: "./src/index.html",
+            chunks: ["vendor", "app"],
+            chunksSortMode: "manual",
             minify: {
                 removeAttributeQuotes: false,
                 collapseWhitespace: false,
@@ -53,21 +50,25 @@ module.exports = {
             hash: false
         }),
         new BrowserSyncPlugin({
-            host: process.env.IP || 'localhost',
+            host: process.env.IP || "localhost",
             port: process.env.PORT || 3000,
             server: {
-                baseDir: ['./', './dev']
+                baseDir: ["./", "./dev"]
             }
         })
     ],
     module: {
         rules: [
-            { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
+            {
+                test: /\.js$/,
+                use: ["babel-loader"],
+                include: path.join(__dirname, "src")
+            },
+            { test: /phaser-split\.js$/, use: ["expose-loader?Phaser"] },
+            { test: [/\.vert$/, /\.frag$/], use: "raw-loader" }
         ]
-    },
-   /* node: {
+    }
+    /* node: {
         fs: 'empty',
         net: 'empty',
         tls: 'empty'
@@ -77,4 +78,4 @@ module.exports = {
             'phaser': phaser,
         }
     }*/
-}
+};
